ae3ed98cbbb98c487e5dacc80aa075381d85cbc5
Wilson Vidyut Doloy <54664361+Wilson0406@users.noreply.github.com>
2022-02-03 23:47:54 +0530

    updated all the functionalities

--- Blackjack Game/index.js
+++ Blackjack Game/index.js
@@ -1,9 +1,7 @@
-let firstCard = 10
-let secondCard = 4
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 //let sumEl = document.querySelector("#sum-el") if sum-el is an ID
@@ -11,14 +9,44 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+console.log(cards)
 
-function startGame(){
-  renderGame();
+// object for player
+let player = {
+    name: "Wilson",
+    chips: 100
+}
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
+//to generate random numbers
+function getRandomCard() {
+    // let randomNumer = Math.ceil( Math.random()*13 ) - same as below
+    let randomNumer = Math.floor( Math.random()*13 ) + 1 //random number between (0 and 12)+1
+    if (randomNumer > 10) {
+        return 10
+    } else if (randomNumer === 1) {
+        return 11
+    } else {
+        return randomNumer
+    }
+}
+
+function startGame() {
+    if(player.chips >0){
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame();
+    }
 }
 
 function renderGame() {
     cardsEl.textContent = "Cards: "
-    // for loop that renders out all the cards instead of just two
+    // Create a for loop that renders out all the cards instead of just two
     for (let i = 0; i < cards.length; i++) {
         cardsEl.textContent += cards[i] + " "
     } 
@@ -29,20 +57,25 @@ function renderGame() {
 } else if (sum === 21) {
     message = "Wohoo! You've got Blackjack! 🥳"
     hasBlackJack = true
+    player.chips += 20
+    playerEl.textContent = player.name + ": $" + player.chips
 } else {
     message = "You're out of the game! 😭"
     isAlive = false
+    player.chips -= 20
+    playerEl.textContent = player.name + ": $" + player.chips
 }
   messageEl.textContent = message
 console.log(message)
 }
 
 function newCard() {
+    if(isAlive === true && hasBlackJack === false && player.chips > 0){
     let card = 6
     console.log("Drawing a new card from the deck!")
     sum += card
     // Push the card to the cards array
     cards.push(card)
     console.log(cards)
-    renderGame()
+    renderGame()}
 }
