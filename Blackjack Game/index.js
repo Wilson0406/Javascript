ee447ede85d3b0175595d315daa6e7142817b7bf
Wilson Vidyut Doloy <54664361+Wilson0406@users.noreply.github.com>
2022-02-03 13:52:45 +0530

    update index.js

--- Blackjack Game/index.js
+++ Blackjack Game/index.js
@@ -1,1 +1,24 @@
-
+let firstCard = 10
+let secondCard = 4
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+//let sumEl = document.querySelector("#sum-el") if sum-el is an ID
+//let sumEl = document.querySelector(".sum-el") if sum-el is a Class
+let sumEl = document.getElementById("sum-el")
+function startGame(){
+      sumEl.textContent = "Sum: " + sum;
+  if (sum <= 20) {
+    message = "Do you want to draw a new card? 🙂"
+} else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳"
+    hasBlackJack = true
+} else {
+    message = "You're out of the game! 😭"
+    isAlive = false
+}
+  messageEl.textContent = message
+console.log(message)
+}
