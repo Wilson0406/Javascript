f7232754209ffdea4c5a363b363b2d583588d653
Wilson Vidyut Doloy <54664361+Wilson0406@users.noreply.github.com>
2022-02-03 13:48:54 +0530

    update index.js

--- Passenger Count App/index.js
+++ Passenger Count App/index.js
@@ -1,1 +1,22 @@
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
 
+console.log(countEl);
+console.log(saveEl);
+
+let count = 0;
+
+function increment() {
+    count += 1;
+    //countEl.innerText = count;
+    countEl.textContent = count;
+}
+
+function save() {
+    let countStr = count + " - ";
+    //saveEl.innerText += countStr
+    saveEl.textContent += countStr;
+    console.log(count);
+    countEl.textContent = 0;
+    count = 0;
+}
