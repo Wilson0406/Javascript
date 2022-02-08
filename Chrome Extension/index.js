3dfc889478032afbb98266fe98d16c4e9e3ac58d
Wilson Vidyut Doloy <54664361+Wilson0406@users.noreply.github.com>
2022-02-08 13:20:37 +0530

    input field working

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -1,20 +1,24 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
+    inputEl.value = ""
     console.log(myLeads)
+    renderLeads();
 })
 
-// Let's try a different method!
-for (let i = 0; i < myLeads.length; i++) {
-     ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-    /* create element
-     set text content
-     append to ul
-    const li = document.createElement("li")
-    li.textContent = myLeads[i]
-    ulEl.append(li)*/
+function renderLeads(){
+  let listItems = ""
+  for (let i = 0; i < myLeads.length; i++) {
+      listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+      //ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+      /* create element set text content append to ul
+      const li = document.createElement("li")
+      li.textContent = myLeads[i]
+      ulEl.append(li)*/
+  }
+  ulEl.innerHTML = listItems
 }
