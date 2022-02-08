8e22f569c3fcc787c32e3deaaa7fd47e56c6f26e
Wilson Vidyut Doloy <54664361+Wilson0406@users.noreply.github.com>
2022-02-08 13:28:33 +0530

    added template strings
    template strings lets you add html code in js without any quotes("" or '')

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -6,19 +6,21 @@ const ulEl = document.getElementById("ul-el")
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
-    console.log(myLeads)
-    renderLeads();
+    renderLeads()
 })
 
-function renderLeads(){
-  let listItems = ""
-  for (let i = 0; i < myLeads.length; i++) {
-      listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
-      //ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-      /* create element set text content append to ul
-      const li = document.createElement("li")
-      li.textContent = myLeads[i]
-      ulEl.append(li)*/
-  }
-  ulEl.innerHTML = listItems
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+     // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+      //template strings or literals: https://www.w3schools.com/js/js_string_templates.asp  
+      listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                  ${myLeads[i]}
+                </a>
+            </li>
+    `
+    }
+    ulEl.innerHTML = listItems  
 }
