0bfec5193e8253d1f15ea93d3cf1a1f337001865
Wilson Vidyut Doloy <54664361+Wilson0406@users.noreply.github.com>
2022-02-08 14:58:16 +0530

    JSON methods to store array in localStorage

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -3,6 +3,26 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+// localStorage.setItem(key, value)
+// localStorage.getItem(key)
+// localStorage.clear()
+// PS: both key and value need to be strings
+
+
+// https://stackoverflow.com/questions/3357553/how-do-i-store-an-array-in-localstorage
+/*
+let myLeads = `["www.awesomelead.com"]`
+
+// 1. Turn the myLeads string into an array
+myLeads = JSON.parse(myLeads)
+// 2. Push a new value to the array
+myLeads.push("www.lead2.com")
+// 3. Turn the array into a string again
+myLeads = JSON.stringify(myLeads)
+// 4. Console.log the string using typeof to verify that it's a string
+console.log(typeof myLeads)
+*/
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
