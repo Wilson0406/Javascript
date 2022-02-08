457ee14195d04135c3cd94e1d6a23ca6a61349cd
Wilson Vidyut Doloy <54664361+Wilson0406@users.noreply.github.com>
2022-02-08 15:01:29 +0530

    save myLeads to localStorage

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -3,44 +3,28 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
-// localStorage.setItem(key, value)
-// localStorage.getItem(key)
-// localStorage.clear()
-// PS: both key and value need to be strings
-
-
-// https://stackoverflow.com/questions/3357553/how-do-i-store-an-array-in-localstorage
-/*
-let myLeads = `["www.awesomelead.com"]`
-
-// 1. Turn the myLeads string into an array
-myLeads = JSON.parse(myLeads)
-// 2. Push a new value to the array
-myLeads.push("www.lead2.com")
-// 3. Turn the array into a string again
-myLeads = JSON.stringify(myLeads)
-// 4. Console.log the string using typeof to verify that it's a string
-console.log(typeof myLeads)
-*/
-
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    // Save the myLeads array to localStorage 
+    // PS: remember JSON.stringify()
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     renderLeads()
+    
+    // To verify that it works:
+    console.log( localStorage.getItem("myLeads") )
 })
 
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-     // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
-      //template strings or literals: https://www.w3schools.com/js/js_string_templates.asp  
-      listItems += `
+        listItems += `
             <li>
                 <a target='_blank' href='${myLeads[i]}'>
-                  ${myLeads[i]}
+                    ${myLeads[i]}
                 </a>
             </li>
-    `
+        `
     }
     ulEl.innerHTML = listItems  
 }
