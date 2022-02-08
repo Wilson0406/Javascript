9f4cece70ad2e9738f780a3c02854ebe8857e88c
Wilson Vidyut Doloy <54664361+Wilson0406@users.noreply.github.com>
2022-02-08 15:24:22 +0530

    getting myLeads from Local Storage

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -3,6 +3,17 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+// ["lead1", "lead2"] or null
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+console.log(leadsFromLocalStorage)
+// 1. Check if leadsFromLocalStorage is truthy
+// 2. If so, set myLeads to its value and call renderLeads()
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
