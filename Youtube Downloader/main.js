854ce4c035e239b2705fa439ee2f89c4a0c4f4d8
Wilson Vidyut Doloy <54664361+Wilson0406@users.noreply.github.com>
2022-06-07 00:58:22 +0530

    Update main.js

--- Youtube Downloader/main.js
+++ Youtube Downloader/main.js
@@ -1,1 +1,13 @@
 
+
+$(".btn").click(function () {
+    var link = $(".link").val();
+    var format = $(".format").children("option:selected").val();
+    var src = "" + link + "=" + format + "";
+    download(link, format);
+});
+
+function download(link, format) {
+    // using loader.to api
+    $('.button-container').html(`<iframe style="height:50px; border:none; overflow:hidden;" src="https://loader.to/api/button?url=` + link + `&f` + format + `&color=ee4b4b&adUrl=https://wilson-vd.netlify.app"></iframe>`);  
+}
